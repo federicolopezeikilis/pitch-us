3ec835d407ff4750ee99bf9dc191e3a486179ba8
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-06-30 00:12:27 +0200

    remove changes on verifyTokenWithApiCall

--- app/helpers/verifyTokenWithAPICall.js
+++ app/helpers/verifyTokenWithAPICall.js
@@ -3,36 +3,14 @@ import Apium from '../vendor/Apium'
 import { context } from '../logic'
 
 export async function verifyTokenWithAPICall(req, res) {
-    const errorHeroku = `<!DOCTYPE html>
-	<html>
-	  <head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta charset="utf-8">
-		<title>No such app</title>
-		<style media="screen">
-		  html,body,iframe {
-			margin: 0;
-			padding: 0;
-		  }
-		  html,body {
-			height: 100%;
-			overflow: hidden;
-		  }
-		  iframe {
-			width: 100%;
-			height: 100%;
-			border: 0;
-		  }
-		</style>
-	  </head>
-	  <body>
-		<iframe src="//www.herokucdn.com/error-pages/no-such-app.html"></iframe>
-	  </body>
-	</html>`
+    const cookies = new Cookies(req, res)
 
-    async function callApi() {
+    const token = cookies.get('token')
+    console.log(token || 'no hay token')
+    if (token) {
+        console.log('line 11')
         const api = new Apium(context.API_URL)
-        console.log('api call to do')
+
         const { status, payload } = await api.get('users/auth', {
             headers: {
                 Authorization: `Bearer ${token}`
@@ -52,14 +30,9 @@ export async function verifyTokenWithAPICall(req, res) {
 
                 return { token, userId }
             }
-        } else if (status === 404 && payload === errorHeroku) {
-            console.log('heroku error catch')
-            
-            callApi()
-
         } else if (status === 401 || status === 404) {
             console.log('line 34')
-
+            
             cookies.set('token')
 
             if (req.url.includes('/profile/settings')
@@ -77,23 +50,13 @@ export async function verifyTokenWithAPICall(req, res) {
         }
     }
 
-    const cookies = new Cookies(req, res)
-
-    const token = cookies.get('token')
-    console.log(token || 'no hay token')
-    if (token) {
-        console.log('line 11')
-
-        callApi()
-    }
-
     if (req.url.includes('/profile/settings')
         || req.url.includes('/profile/edit')
         || req.url.includes('/profile/change-password')
         || req.url.includes('/profile/delete-account')
         || req.url.includes('/create-interpretation')) {
 
-        console.log('line 59')
+            console.log('line 59')
 
 
         res.writeHead(307, { Location: '/login' })
