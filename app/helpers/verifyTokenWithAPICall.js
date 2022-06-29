f2fc141f785023a50d3b27bbac0c1a12acd558aa
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-06-29 23:54:46 +0200

    recursivity verify token

--- app/helpers/verifyTokenWithAPICall.js
+++ app/helpers/verifyTokenWithAPICall.js
@@ -9,43 +9,47 @@ export async function verifyTokenWithAPICall(req, res) {
     console.log(token || 'no hay token')
     if (token) {
         console.log('line 11')
-        const api = new Apium(context.API_URL)
+        function callApi() {
+            const api = new Apium(context.API_URL)
 
-        const { status, payload } = await api.get('users/auth', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-        if (status === 200) {
-            console.log('line 21')
-            if (req.url.includes('/login') || req.url.includes('/register')) {
-                console.log('line 23')
-                res.writeHead(307, { Location: '/' })
-                res.end()
-            } else {
-                console.log('line 27')
-
-                const { userId } = JSON.parse(payload)
-
-                return { token, userId }
-            }
-        } else if (status === 401 || status === 404) {
-            console.log('line 34')
-            
-            cookies.set('token')
-
-            if (req.url.includes('/profile/settings')
-                || req.url.includes('/profile/edit')
-                || req.url.includes('/profile/change-password')
-                || req.url.includes('/profile/delete-account')
-                || req.url.includes('/create-interpretation')) {
-
-                console.log('line 44')
-
-                res.writeHead(307, { Location: '/login' })
-                res.end()
-                return
+            const { status, payload } = await api.get('users/auth', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+    
+            if (status === 200) {
+                console.log('line 21')
+                if (req.url.includes('/login') || req.url.includes('/register')) {
+                    console.log('line 23')
+                    res.writeHead(307, { Location: '/' })
+                    res.end()
+                } else {
+                    console.log('line 27')
+    
+                    const { userId } = JSON.parse(payload)
+    
+                    return { token, userId }
+                }
+            } else if (status === 401) {
+                console.log('line 34')
+                
+                cookies.set('token')
+    
+                if (req.url.includes('/profile/settings')
+                    || req.url.includes('/profile/edit')
+                    || req.url.includes('/profile/change-password')
+                    || req.url.includes('/profile/delete-account')
+                    || req.url.includes('/create-interpretation')) {
+    
+                    console.log('line 44')
+    
+                    res.writeHead(307, { Location: '/login' })
+                    res.end()
+                    return
+                }
+            } else if (status === 404) {
+                callApi()
             }
         }
     }
